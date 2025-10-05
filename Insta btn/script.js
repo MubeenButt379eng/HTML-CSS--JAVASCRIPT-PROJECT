4dc3b8b8bc870bf3da4721c99ff3fdaebe33d329
MubeenButt379eng <mubeenbutt375@gmail.com>
2025-10-05 21:16:18 +0500

    insta like btn

---
+++ Insta btn/script.js
@@ -0,0 +1,15 @@
+let cont = document.querySelector(".container")
+let icon = document.querySelector("i")
+
+cont.addEventListener("dblclick",function(){
+    icon.style.transform = "translate(-50%,-50%) scale(1)"
+    icon.style.opacity = 0.8;
+    setTimeout(() => {
+        icon.style.opacity = 0;
+    }, 1000);
+    setTimeout(() => {
+        icon.style.transform = "translate(-50%,-50%) scale(0)"
+    }, 2000);
+})
+
+ 
